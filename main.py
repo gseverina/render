a01d5bb910f31744d2ad0708501676715558195d
Gaston Severina <gseverina@mperativ.io>
2025-07-30 11:33:27 -0300

    mv

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello Render!"}
+
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
